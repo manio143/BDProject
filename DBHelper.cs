f80aad7137611e39fade79196fad7c4faa50d552
manio143 <marian.dziubiak@gmail.com>
2016-11-28 15:46:25 +0100

    Add empty ConnectionString and DBHelper

---
+++ DBHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Common;
+using Oracle.ManagedDataAccess.Client;
+using Dapper;
+
+namespace BDProject
+{
+	public static class DBHelper
+	{
+		public static DbConnection Open() {
+			return new OracleConnection (ConfigurationManager.ConnectionStrings["DbConnectionString"].ConnectionString);
+		}
+	}
+}
+
