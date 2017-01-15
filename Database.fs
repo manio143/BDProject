f0155e03dcb690271c171c4871cf8740299204fa
Marian Dziubiak <marian.dziubiak@gmail.com>
2017-01-15 22:27:34 +0100

    Migrate to FSharp

---
+++ Database.fs
@@ -0,0 +1,27 @@
+﻿module Database
+
+    open FSharp.Data.Sql    
+
+    let [<Literal>] private resolutionPath = __SOURCE_DIRECTORY__ + "/bin/Debug/"
+
+    type mySql = SqlDataProvider<
+                        ConnectionString = "Server=localhost;Database=inf;Uid=root;Pwd=root;",
+                        DatabaseVendor = Common.DatabaseProviderTypes.MYSQL,
+                        ResolutionPath = resolutionPath,
+                        IndividualsAmount = 1000,
+                        UseOptionTypes = true >
+
+    //type oracleSql = SqlDataProvider<
+    //                    ConnectionString = "DataSource=myOracleDb;User Id=md370784;Pwd=Meninblack1;",
+    //                    DatabaseVendor = Common.DatabaseProviderTypes.ORACLE,
+    //                    ResolutionPath = resolutionPath,
+    //                    IndividualsAmount = 1000,
+    //                    UseOptionTypes = true >
+                        
+    type SqlDatabase = MySQL | Oracle
+
+    let openDb db = 
+        match db with
+        | MySQL -> mySql.GetDataContext()
+        | Oracle -> raise (new System.NotImplementedException())//oracleSql.GetDataContext()
+
---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿open Database
+
+[<EntryPoint>]
+let main argv = 
+    let ctx = openDb MySQL
+    let example =
+            query {
+                for i in ctx.Inf.Test do
+                    select (i.Id)
+                  }
+    example |> Seq.iter (fun x -> printfn "%d" x)
+    0 // return an integer exit code
+
