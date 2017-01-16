dc1fd47dddf3bb82e217acc37b517d291ce89e80
Marian Dziubiak <marian.dziubiak@gmail.com>
2017-01-16 10:06:32 +0100

    Migrate to Dapper

--- DataAccess.fs
+++ DataAccess.fs
@@ -2,4 +2,9 @@
 
     open Database
 
+    let cnn = ``open`` MySQL
+
+    type TestValue = {Id: int}
+
+    let test () = printfn "%A" (query<TestValue> cnn "SELECT id FROM inf.test;")
 //TODO: create functions for the buisness logic of the app
--- Program.fs
+++ Program.fs
@@ -3,6 +3,7 @@ open Suave
 
 [<EntryPoint>]
 let main argv = 
+    DataAccess.test()
     startWebServer defaultConfig Web.mainApp
     0 // return an integer exit code
 
